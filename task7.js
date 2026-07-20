3f518e9da71dc208e96e61b0105f6419f612a6d6
syelnn <axioohype5familygmail.com>
2026-07-20 10:26:51 +0700

    feat: selessaikan task7

---
+++ task7.js
@@ -0,0 +1,64 @@
+const authors = [
+  {
+    id: 1,
+    name: "Robert C. Martin",
+    country: "USA"
+  },
+  {
+    id: 2,
+    name: "James Clear",
+    country: "USA"
+  },
+  {
+    id: 3,
+    name: "Marijn Haverbeke",
+    country: "Netherlands"
+  },
+  {
+    id: 4,
+    name: "Andrea Hirata",
+    country: "Indonesia"
+  }
+];
+
+const books = [
+  {
+    id: 1,
+    title: "Clean Code",
+    authorId: 1,
+    year: 2008,
+    available: true
+  },
+  {
+    id: 2,
+    title: "Atomic Habits",
+    authorId: 2,
+    year: 2018,
+    available: false
+  },
+  {
+    id: 3,
+    title: "Eloquent JavaScript",
+    authorId: 3,
+    year: 2019,
+    available: true
+  },
+  {
+    id: 4,
+    title: "Laskar Pelangi",
+    authorId: 4,
+    year: 2005,
+    available: true
+  }
+];
+
+authors.forEach(author => {
+  // Hitung jumlah buku milik author saat ini
+  const authorBooks = books.filter(book => book.authorId === author.id);
+  const jumlahBuku = authorBooks.length;
+
+  // Menampilkan output
+  console.log(author.name);
+  console.log(`Jumlah Buku : ${jumlahBuku}`);
+  console.log(""); // Baris kosong jarak antar statistik penulis
+});
